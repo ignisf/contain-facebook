14877a086ef34ca221ae3d33774503cbe7352b7a
Petko Bordjukov <bordjukov@gmail.com>
2018-03-28 12:36:19 +0300

    Convert to a LinkedIn container extension

--- background.js
+++ background.js
@@ -1,16 +1,53 @@
 // Param values from https://developer.mozilla.org/Add-ons/WebExtensions/API/contextualIdentities/create
-const FACEBOOK_CONTAINER_NAME = "Facebook";
-const FACEBOOK_CONTAINER_COLOR = "blue";
-const FACEBOOK_CONTAINER_ICON = "briefcase";
-const FACEBOOK_DOMAINS = ["facebook.com", "www.facebook.com", "fb.com"];
+const LINKEDIN_CONTAINER_NAME = "LinkedIn";
+const LINKEDIN_CONTAINER_COLOR = "blue";
+const LINKEDIN_CONTAINER_ICON = "fingerprint";
+
+const LINKEDIN_TLD = "linkedin.com";
+const LINKEDIN_SUBDOMAINS = ['af', 'ax', 'al', 'dz', 'as', 'ad', 'ao', 'ai',
+                             'aq', 'ag', 'ar', 'am', 'aw', 'au', 'at', 'az',
+                             'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 'bj',
+                             'bm', 'bt', 'bo', 'bq', 'ba', 'bw', 'bv', 'br',
+                             'io', 'bn', 'bg', 'bf', 'bi', 'cv', 'kh', 'cm',
+                             'ca', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc',
+                             'co', 'km', 'cg', 'cd', 'ck', 'cr', 'ci', 'hr',
+                             'cu', 'cw', 'cy', 'cz', 'dk', 'dj', 'dm', 'do',
+                             'ec', 'eg', 'sv', 'gq', 'er', 'ee', 'et', 'fk',
+                             'fo', 'fj', 'fi', 'fr', 'gf', 'pf', 'tf', 'ga',
+                             'gm', 'ge', 'de', 'gh', 'gi', 'gr', 'gl', 'gd',
+                             'gp', 'gu', 'gt', 'gg', 'gn', 'gw', 'gy', 'ht',
+                             'hm', 'va', 'hn', 'hk', 'hu', 'is', 'in', 'id',
+                             'ir', 'iq', 'ie', 'im', 'il', 'it', 'jm', 'jp',
+                             'je', 'jo', 'kz', 'ke', 'ki', 'kp', 'kr', 'kw',
+                             'kg', 'la', 'lv', 'lb', 'ls', 'lr', 'ly', 'li',
+                             'lt', 'lu', 'mo', 'mk', 'mg', 'mw', 'my', 'mv',
+                             'ml', 'mt', 'mh', 'mq', 'mr', 'mu', 'yt', 'mx',
+                             'fm', 'md', 'mc', 'mn', 'me', 'ms', 'ma', 'mz',
+                             'mm', 'na', 'nr', 'np', 'nl', 'nc', 'nz', 'ni',
+                             'ne', 'ng', 'nu', 'nf', 'mp', 'no', 'om', 'pk',
+                             'pw', 'ps', 'pa', 'pg', 'py', 'pe', 'ph', 'pn',
+                             'pl', 'pt', 'pr', 'qa', 're', 'ro', 'ru', 'rw',
+                             'bl', 'sh', 'kn', 'lc', 'mf', 'pm', 'vc', 'ws',
+                             'sm', 'st', 'sa', 'sn', 'rs', 'sc', 'sl', 'sg',
+                             'sx', 'sk', 'si', 'sb', 'so', 'za', 'gs', 'ss',
+                             'es', 'lk', 'sd', 'sr', 'sj', 'sz', 'se', 'ch',
+                             'sy', 'tw', 'tj', 'tz', 'th', 'tl', 'tg', 'tk',
+                             'to', 'tt', 'tn', 'tr', 'tm', 'tc', 'tv', 'ug',
+                             'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu',
+                             've', 'vn', 'vg', 'vi', 'wf', 'eh', 'ye', 'zm',
+                             'zw', 'www']
+
+const LINKEDIN_DOMAINS = [LINKEDIN_TLD].concat(LINKEDIN_SUBDOMAINS.map(function(subdomain){
+    return(subdomain + '.' + LINKEDIN_TLD);
+}));
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
-let facebookCookieStoreId = null;
+let linkedInCookieStoreId = null;
 
-const facebookHostREs = [];
+const linkedInHostREs = [];
 
-async function isFacebookAlreadyAssignedInMAC () {
+async function isLinkedInAlreadyAssignedInMAC () {
   let macAddonInfo;
   // If the MAC add-on isn't installed, return false
   try {
@@ -19,11 +56,11 @@ async function isFacebookAlreadyAssignedInMAC () {
     return false;
   }
   let anyFBDomainsAssigned = false;
-  for (let facebookDomain of FACEBOOK_DOMAINS) {
-    const facebookCookieUrl = `https://${facebookDomain}/`;
+  for (let linkedInDomain of LINKEDIN_DOMAINS) {
+    const linkedInCookieUrl = `https://${linkedInDomain}/`;
     const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
       method: "getAssignment",
-      url: facebookCookieUrl
+      url: linkedInCookieUrl
     });
     if (assignment) {
       anyFBDomainsAssigned = true;
@@ -33,63 +70,63 @@ async function isFacebookAlreadyAssignedInMAC () {
 }
 
 (async function init() {
-  const facebookAlreadyAssigned = await isFacebookAlreadyAssignedInMAC();
-  if (facebookAlreadyAssigned) {
+  const linkedInAlreadyAssigned = await isLinkedInAlreadyAssignedInMAC();
+  if (linkedInAlreadyAssigned) {
     return;
   }
 
-  // Clear all facebook cookies
-  for (let facebookDomain of FACEBOOK_DOMAINS) {
-    facebookHostREs.push(new RegExp(`^(.*)?${facebookDomain}$`));
-    const facebookCookieUrl = `https://${facebookDomain}/`;
+  // Clear all linkedIn cookies
+  for (let linkedInDomain of LINKEDIN_DOMAINS) {
+    linkedInHostREs.push(new RegExp(`^(.*)?${linkedInDomain}$`));
+    const linkedInCookieUrl = `https://${linkedInDomain}/`;
 
-    browser.cookies.getAll({domain: facebookDomain}).then(cookies => {
+    browser.cookies.getAll({domain: linkedInDomain}).then(cookies => {
       for (let cookie of cookies) {
-        browser.cookies.remove({name: cookie.name, url: facebookCookieUrl});
+        browser.cookies.remove({name: cookie.name, url: linkedInCookieUrl});
       }
     });
   }
 
-  // Use existing Facebook container, or create one
-  browser.contextualIdentities.query({name: FACEBOOK_CONTAINER_NAME}).then(contexts => {
+  // Use existing LinkedIn container, or create one
+  browser.contextualIdentities.query({name: LINKEDIN_CONTAINER_NAME}).then(contexts => {
     if (contexts.length > 0) {
-      facebookCookieStoreId = contexts[0].cookieStoreId;
+      linkedInCookieStoreId = contexts[0].cookieStoreId;
     } else {
       browser.contextualIdentities.create({
-        name: FACEBOOK_CONTAINER_NAME,
-        color: FACEBOOK_CONTAINER_COLOR,
-        icon: FACEBOOK_CONTAINER_ICON}
+        name: LINKEDIN_CONTAINER_NAME,
+        color: LINKEDIN_CONTAINER_COLOR,
+        icon: LINKEDIN_CONTAINER_ICON}
       ).then(context => {
-        facebookCookieStoreId = context.cookieStoreId;
+        linkedInCookieStoreId = context.cookieStoreId;
       });
     }
   });
 
-  // Listen to requests and open Facebook into its Container,
+  // Listen to requests and open LinkedIn into its Container,
   // open other sites into the default tab context
-  async function containFacebook(options) {
+  async function containLinkedIn(options) {
     const requestUrl = new URL(options.url);
-    let isFacebook = false;
-    for (let facebookHostRE of facebookHostREs) {
-      if (facebookHostRE.test(requestUrl.host)) {
-        isFacebook = true;
+    let isLinkedIn = false;
+    for (let linkedInHostRE of linkedInHostREs) {
+      if (linkedInHostRE.test(requestUrl.host)) {
+        isLinkedIn = true;
         break;
       }
     }
     const tab = await browser.tabs.get(options.tabId);
     const tabCookieStoreId = tab.cookieStoreId;
-    if (isFacebook) {
-      if (tabCookieStoreId !== facebookCookieStoreId && !tab.incognito) {
-        // See https://github.com/mozilla/contain-facebook/issues/23
+    if (isLinkedIn) {
+      if (tabCookieStoreId !== linkedInCookieStoreId && !tab.incognito) {
+        // See https://github.com/mozilla/contain-firefox/issues/23
         // Sometimes this add-on is installed but doesn't get a facebookCookieStoreId ?
-        if (facebookCookieStoreId) {
-          browser.tabs.create({url: requestUrl.toString(), cookieStoreId: facebookCookieStoreId});
+        if (linkedInCookieStoreId) {
+          browser.tabs.create({url: requestUrl.toString(), cookieStoreId: linkedInCookieStoreId});
           browser.tabs.remove(options.tabId);
           return {cancel: true};
         }
       }
     } else {
-      if (tabCookieStoreId === facebookCookieStoreId) {
+      if (tabCookieStoreId === linkedInCookieStoreId) {
         browser.tabs.create({url: requestUrl.toString()});
         browser.tabs.remove(options.tabId);
         return {cancel: true};
@@ -98,5 +135,5 @@ async function isFacebookAlreadyAssignedInMAC () {
   }
 
   // Add the request listener
-  browser.webRequest.onBeforeRequest.addListener(containFacebook, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
+  browser.webRequest.onBeforeRequest.addListener(containLinkedIn, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
 })();
